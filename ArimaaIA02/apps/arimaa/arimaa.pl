34bfbfa197342a14c850cbbbe0dcb01e99a208bd
SoLamrous <solamrous@gmail.com>
2017-05-24 11:44:31 +0200

    v0.0.0.1

--- ArimaaIA02/apps/arimaa/arimaa.pl
+++ ArimaaIA02/apps/arimaa/arimaa.pl
@@ -1,6 +1,7 @@
 :- module(bot,
       [  get_moves/3
       ]).
+
 	
 % A few comments but all is explained in README of github
 
@@ -12,7 +13,21 @@
 % board: [[0,0,rabbit,silver],[0,1,rabbit,silver],[0,2,horse,silver],[0,3,rabbit,silver],[0,4,elephant,silver],[0,5,rabbit,silver],[0,6,rabbit,silver],[0,7,rabbit,silver],[1,0,camel,silver],[1,1,cat,silver],[1,2,rabbit,silver],[1,3,dog,silver],[1,4,rabbit,silver],[1,5,horse,silver],[1,6,dog,silver],[1,7,cat,silver],[2,7,rabbit,gold],[6,0,cat,gold],[6,1,horse,gold],[6,2,camel,gold],[6,3,elephant,gold],[6,4,rabbit,gold],[6,5,dog,gold],[6,6,rabbit,gold],[7,0,rabbit,gold],[7,1,rabbit,gold],[7,2,rabbit,gold],[7,3,cat,gold],[7,4,dog,gold],[7,5,rabbit,gold],[7,6,horse,gold],[7,7,rabbit,gold]]
 
 % Call exemple:
-% get_moves(Moves, [silver, []], [[0,0,rabbit,silver],[0,1,rabbit,silver],[0,2,horse,silver],[0,3,rabbit,silver],[0,4,elephant,silver],[0,5,rabbit,silver],[0,6,rabbit,silver],[0,7,rabbit,silver],[1,0,camel,silver],[1,1,cat,silver],[1,2,rabbit,silver],[1,3,dog,silver],[1,4,rabbit,silver],[1,5,horse,silver],[1,6,dog,silver],[1,7,cat,silver],[2,7,rabbit,gold],[6,0,cat,gold],[6,1,horse,gold],[6,2,camel,gold],[6,3,elephant,gold],[6,4,rabbit,gold],[6,5,dog,gold],[6,6,rabbit,gold],[7,0,rabbit,gold],[7,1,rabbit,gold],[7,2,rabbit,gold],[7,3,cat,gold],[7,4,dog,gold],[7,5,rabbit,gold],[7,6,horse,gold],[7,7,rabbit,gold]]).
+ get_moves(Moves, [silver, []], [[0,0,rabbit,silver],[0,1,rabbit,silver],[0,2,horse,silver],[0,3,rabbit,silver],[0,4,elephant,silver],[0,5,rabbit,silver],[0,6,rabbit,silver],[0,7,rabbit,silver],[1,0,camel,silver],[1,1,cat,silver],[1,2,rabbit,silver],[1,3,dog,silver],[1,4,rabbit,silver],[1,5,horse,silver],[1,6,dog,silver],[1,7,cat,silver],[2,7,rabbit,gold],[6,0,cat,gold],[6,1,horse,gold],[6,2,camel,gold],[6,3,elephant,gold],[6,4,rabbit,gold],[6,5,dog,gold],[6,6,rabbit,gold],[7,0,rabbit,gold],[7,1,rabbit,gold],[7,2,rabbit,gold],[7,3,cat,gold],[7,4,dog,gold],[7,5,rabbit,gold],[7,6,horse,gold],[7,7,rabbit,gold]]).
 
 % default call
-get_moves([[[1,0],[2,0]],[[0,0],[1,0]],[[0,1],[0,0]],[[0,0],[0,1]]], Gamestate, Board).
+
+get_moves(Mo, Gamestate, Board):-element([1,0,_,silver],Board),nonmember([3,0,_],Board),!,Mo=[[[1,0],[3,0]],[[1,1],[3,1]]].
+get_moves(Mo, Gamestate, Board):-element([3,0,_,silver],Board),nonmember([4,0,_],Board),!,Mo=[[[3,0],[4,0]],[[3,1],[4,1]]].
+get_moves(Mo, Gamestate, Board):-Mo=[[[4,0],[3,0]],[[4,1],[3,1]]].
+
+element(X,[X|Q]).
+element(X,[T|Q]) :- element(X,Q).
+
+nonmember(Arg,[Arg|_]) :-
+        !,
+        fail.
+nonmember(Arg,[_|Tail]) :-
+        !,
+        nonmember(Arg,Tail).
+nonmember(_,[]).
